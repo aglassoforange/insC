da7a09acb2b177cb3560513b1b9594be5cede551
yaoliu <yliu2911@gmail.com>
2019-12-19 18:27:40 +0800

    add comment and count and error catching system

--- ins.py
+++ ins.py
@@ -1,27 +1,21 @@
 #coding=utf-8
+#这是一个抓取解析ins 个人主页的程序
+
 import requests
 import re
 import urllib.request
 import json
 
 class Crawl_ins():
+
     outputlist = []
     has_next_page = True
     cursor=""
     comment_cusor=""
-    count = 0
     comment_output = []
     has_next_comment_page = True
-    test_count = 0
-    def crawl_ins(url):
-        headers = {
-            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 "
-                          "(KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
-        }
-        reponse = requests.get(url, headers=headers)
-        data = reponse.text
-        json_data = json.loads(data)
 
+    #得到个人首页
     def get_id(self, name):
         url = "https://www.instagram.com/"+name+"/"
         headers = {
@@ -34,44 +28,52 @@ class Crawl_ins():
             'accept-language': 'zh-CN,zh;q=0.9',
             'cache-control': 'max-age=0'
         }
+        try:
+            response = requests.get(url, headers=headers)
+            return response.text
+        except Exception as e:
+             print(e)
 
-        response = requests.get(url, headers=headers)
-        return response.text
-
+    #解析HTML界面
     def html_parse_key(self, file):
         html = re.search(r'<script type="text/javascript">window._sharedData = (.*?);</script>', file).group(1)
         html_json=json.loads(html)
-        edges=html_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']
-        self.has_next_page=html_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page']
-        self.cursor=html_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
-        id = html_json['entry_data']['ProfilePage'][0]['graphql']['user']['id']
-        username = html_json['entry_data']['ProfilePage'][0]['graphql']['user']['username']
-        for edge in edges:
-            self.count+=1
-            id_pic = edge['node']['id']
-            display_url = edge['node']['shortcode']
-            comment_count = edge['node']['edge_media_to_comment']['count']
-            time = edge['node']['taken_at_timestamp']
-            data = {}
-            out_json = data
-            out_json['id_pic'] = id_pic
-            out_json['display_url'] = display_url
-            out_json['comment_count'] = comment_count
-            out_json['time'] = time
-            self.outputlist.append(out_json)
-        return id, username, self.has_next_page, self.cursor ,self.outputlist
+        try:
+            edges=html_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']
+            self.has_next_page=html_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page']
+            self.cursor=html_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
+            id = html_json['entry_data']['ProfilePage'][0]['graphql']['user']['id']
+            username = html_json['entry_data']['ProfilePage'][0]['graphql']['user']['username']
+            test_count = 0
+            for edge in edges:
+                test_count+=1
+                id_pic = edge['node']['id']
+                display_url = edge['node']['shortcode']
+                comment_count = edge['node']['edge_media_to_comment']['count']
+                time = edge['node']['taken_at_timestamp']
+                data = {}
+                out_json = data
+                out_json['id_pic'] = id_pic
+                out_json['display_url'] = display_url
+                out_json['comment_count'] = comment_count
+                out_json['time'] = time
+                self.outputlist.append(out_json)
+            return id, username, self.has_next_page, self.cursor ,self.outputlist,test_count
+        except Exception as e:
+            print(e)
 
-
+    #解析json
     def json_parse_key(self, file ):
         file = str(file)
         result = json.loads(file)
-        if 'errors' not in result:
+        try:
             count=result['data']['user']['edge_owner_to_timeline_media']['count']
             self.has_next_page=result['data']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page']
             self.cursor=result['data']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
             edges = result['data']['user']['edge_owner_to_timeline_media']['edges']
+            test_count = 0
             for edge in edges:
-                self.count += 1
+                test_count+=1
                 id_pic=edge['node']['id']
                 display_url = edge['node']['shortcode']
                 comment_count = edge['node']['edge_media_to_comment']['count']
@@ -83,8 +85,11 @@ class Crawl_ins():
                 out_json['comment_count'] = comment_count
                 out_json['time'] = time
                 self.outputlist.append(out_json)
-            return self.outputlist, count, self.has_next_page, self.cursor
+            return self.outputlist, count, self.has_next_page, self.cursor,test_count
+        except Exception as e:
+            print(e)
 
+    #去下一个json
     def get_next_pagejson(self, end_cursor , id):
             tittle= 'https://www.instagram.com/graphql/query/'
             headers = {
@@ -102,20 +107,30 @@ class Crawl_ins():
                 "query_hash": "e769aa130647d2354c40ea6a439bfc08",
                 "id": str(id), "first": "12", "after": str(self.cursor)
             }
-            response = requests.get(tittle, headers=headers, params=vars)
-            return response
+            try:
+                response = requests.get(tittle, headers=headers, params=vars)
+                return response
+            except Exception as e:
+                print(e)
 
+    #在有下一页的情况下
     def automation_next(self, name):
         cp = Crawl_ins()
         html =cp.get_id(name)
-        id = cp.html_parse_key(html)[0]
-        while cp.has_next_page is True:
-            json=cp.get_next_pagejson(cp.cursor, id)
-            json=json.text
-            cp.json_parse_key(json)
-        print(cp.count)
-        return cp.outputlist
+        answer= cp.html_parse_key(html)
+        id = answer[0]
+        count = answer[5]
+        try:
+            while cp.has_next_page is True:
+                json=cp.get_next_pagejson(cp.cursor, id)
+                json=json.text
+                count1=cp.json_parse_key(json)[4]
+                count +=count1
+            return cp.outputlist,count
+        except Exception as e:
+            print(e)
 
+    #得到一页的评论，输出表格
     def get_page(self,display_url):
         title = 'https://www.instagram.com/p/'+display_url+'/'
         url = '\'/p/'+display_url+'/\''
@@ -129,41 +144,48 @@ class Crawl_ins():
             'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
             'cache-control': 'max-age=0'
         }
-        html = requests.get(title, headers=headers)
+        try:
+            html = requests.get(title, headers=headers)
+        except Exception as e:
+            print(e)
         html = html.text
         html=str(html)
-        midwork = re.search(r'<script type="text/javascript">window.__additionalDataLoaded((.*?));</script>', html).group(2)
-        start = midwork.find(',')
-        end = midwork.find(');</script>')
-        output = (midwork[start+1:end])
-        html_json=json.loads(output)
-        shortcode = html_json['graphql']['shortcode_media']['shortcode']
-        self.comment_cusor = html_json['graphql']['shortcode_media']['edge_media_to_parent_comment']['page_info']['end_cursor']
-        self.has_next_comment_page = html_json['graphql']['shortcode_media']['edge_media_to_parent_comment']['page_info']['has_next_page']
-        edges = html_json['graphql']['shortcode_media']['edge_media_to_parent_comment']['edges']
-        for edge in edges:
-            data = {}
-            self.test_count= self.test_count+1
-            commenter_id = edge['node']['owner']['id']
-            commenter_name = edge['node']['owner']['username']
-            comment = edge['node']['text']
-            time = edge['node']['created_at']
-            profile_pic = edge['node']['owner']['profile_pic_url']
-            comment_liked  = edge['node']['edge_liked_by']['count']
-            commenter_page = 'https://www.instagram.com/'+commenter_name+'/'
-            out_json = data
-            out_json['time'] = time
-            out_json['id'] = commenter_id
-            out_json['name'] = commenter_name
-            out_json['text'] = comment
-            out_json['commenter\'pic'] = profile_pic
-            out_json['comment like quantity'] = comment_liked
-            out_json['commenter\'s profile page'] = commenter_page
-            self.comment_output.append(out_json)
-        print(self.test_count)
-        self.comment_output.insert(0, 'page_link: '+display_url)
 
-
+        try:
+            midwork = re.search(r'<script type="text/javascript">window.__additionalDataLoaded((.*?));</script>', html).group(2)
+            start = midwork.find(',')
+            end = midwork.find(');</script>')
+            output = (midwork[start+1:end])
+            html_json=json.loads(output)
+            shortcode = html_json['graphql']['shortcode_media']['shortcode']
+            self.comment_cusor = html_json['graphql']['shortcode_media']['edge_media_to_parent_comment']['page_info']['end_cursor']
+            self.has_next_comment_page = html_json['graphql']['shortcode_media']['edge_media_to_parent_comment']['page_info']['has_next_page']
+            edges = html_json['graphql']['shortcode_media']['edge_media_to_parent_comment']['edges']
+            test_count = 0
+            for edge in edges:
+                data = {}
+                test_count= test_count+1
+                commenter_id = edge['node']['owner']['id']
+                commenter_name = edge['node']['owner']['username']
+                comment = edge['node']['text']
+                time = edge['node']['created_at']
+                profile_pic = edge['node']['owner']['profile_pic_url']
+                comment_liked  = edge['node']['edge_liked_by']['count']
+                commenter_page = 'https://www.instagram.com/'+commenter_name+'/'
+                out_json = data
+                out_json['time'] = time
+                out_json['id'] = commenter_id
+                out_json['name'] = commenter_name
+                out_json['text'] = comment
+                out_json['commenter\'pic'] = profile_pic
+                out_json['comment like quantity'] = comment_liked
+                out_json['commenter\'s profile page'] = commenter_page
+                self.comment_output.append(out_json)
+            self.comment_output.insert(0, 'page_link: '+display_url)
+            return test_count
+        except Exception as e:
+            print(e)
+    #解析每一页的json 输出表格
     def get_page_json(self,url):
         headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
@@ -180,42 +202,54 @@ class Crawl_ins():
             "shortcode": url, "first": "12", "after": str(self.comment_cusor)
         }
         url = 'https://www.instagram.com/graphql/query/'
-        file = requests.get(url, headers=headers, params=vars)
+        try:
+            file = requests.get(url, headers=headers, params=vars)
+        except Exception as e:
+            print(e)
         file = file.text
         analysis = json.loads(str(file))
-        self.has_next_comment_page = analysis['data']['shortcode_media']['edge_media_to_parent_comment']['page_info']['has_next_page']
-        self.comment_cusor = analysis['data']['shortcode_media']['edge_media_to_parent_comment']['page_info']['end_cursor']
-        edges = analysis['data']['shortcode_media']['edge_media_to_parent_comment']['edges']
-        for edge in edges:
-            data = {}
-            self.test_count = self.test_count+1
-            commenter_id = edge['node']['owner']['id']
-            commenter_name = edge['node']['owner']['username']
-            comment = edge['node']['text']
-            time = edge['node']['created_at']
-            profile_pic = edge['node']['owner']['profile_pic_url']
-            comment_liked = edge['node']['edge_liked_by']['count']
-            commenter_page = 'https://www.instagram.com/' + commenter_name + '/'
-            out_json = data
-            out_json['time'] = time
-            out_json['id'] = commenter_id
-            out_json['name'] = commenter_name
-            out_json['text'] = comment
-            out_json['commenter\'pic'] = profile_pic
-            out_json['comment like quantity'] = comment_liked
-            out_json['commenter\'s profile page'] = commenter_page
-            self.comment_output.append(out_json)
-        self.comment_output.insert(0, 'page_link: ' + url)
+        try:
+            self.has_next_comment_page = analysis['data']['shortcode_media']['edge_media_to_parent_comment']['page_info']['has_next_page']
+            self.comment_cusor = analysis['data']['shortcode_media']['edge_media_to_parent_comment']['page_info']['end_cursor']
+            edges = analysis['data']['shortcode_media']['edge_media_to_parent_comment']['edges']
+            test_count = 0
+            for edge in edges:
+                data = {}
+                test_count = test_count+1
+                commenter_id = edge['node']['owner']['id']
+                commenter_name = edge['node']['owner']['username']
+                comment = edge['node']['text']
+                time = edge['node']['created_at']
+                profile_pic = edge['node']['owner']['profile_pic_url']
+                comment_liked = edge['node']['edge_liked_by']['count']
+                commenter_page = 'https://www.instagram.com/' + commenter_name + '/'
+                out_json = data
+                out_json['time'] = time
+                out_json['id'] = commenter_id
+                out_json['name'] = commenter_name
+                out_json['text'] = comment
+                out_json['commenter\'pic'] = profile_pic
+                out_json['comment like quantity'] = comment_liked
+                out_json['commenter\'s profile page'] = commenter_page
+                self.comment_output.append(out_json)
+            return test_count
+        except Exception as e:
+            print(e)
 
+    #根据页面名称 打开页面，收集留言
     def automation_in_page(self, display_url):
         dp = Crawl_ins()
-        dp.get_page(display_url)
-        while dp.has_next_comment_page:
-            dp.get_page_json(display_url)
-        return dp.comment_output
-
+        count = dp.get_page(display_url)
+        print(count)
+        try:
+            while dp.has_next_comment_page:
+                count=count+dp.get_page_json(display_url)
+            return dp.comment_output, count
+        except Exception as e:
+            print(e)
 
 if __name__=="__main__":
-    cp = Crawl_ins()
-    cp.automation_in_page('B5q86nwAOS32CijU9yq3M_Qp9vuS-JWbVGlu9g0')
-    print(cp.comment_output)
+    ic = Crawl_ins()
+    output,count=ic.automation_next('cathrynli')
+    print(count)
+    print(output)
